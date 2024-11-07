f9f2d280427391ca87fd9fde6ecf49702fa0024c
Michael Paul Coder <mcoder@lucid-code.com>
2024-11-08 08:12:11 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# LucidScribe-Plugout-Web
+ 
+A Lucid Scribe plugout that opens URLs when rapid eye movements are detected.
+
+To configure the URL, open `Web.lsd` in `%UserProfile%/Documents/lucidcode/Lucid Scribe/Plugouts` and edit the `LucidScribeData\Plugout\Url` node.
---
+++ Web/PlugoutHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace lucidcode.LucidScribe.Plugout.Yocto.PowerRelay
+{
+    public class PlugoutHandler : Interface.LucidPlugoutBase
+    {
+        private Boolean Failed = false;
+
+        public override string Name
+        {
+            get { return "Web"; }
+        }
+
+        public override bool Initialize()
+        {
+            return true;
+        }
+
+        public override void Dispose()
+        {
+            return;
+        }
+
+        public override void Trigger()
+        {
+            try
+            {
+                if (Failed) return;
+
+                var settings = new XmlDocument();
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\lucidcode\Lucid Scribe\Plugouts\Web.lsd";
+                settings.Load(path);
+                var url = settings.DocumentElement.SelectSingleNode("Plugout/Description");
+                System.Diagnostics.Process.Start(url.InnerText);
+            }
+            catch (Exception ex)
+            {
+                Failed = true;
+                MessageBox.Show(ex.Message, "Web Plugout Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
